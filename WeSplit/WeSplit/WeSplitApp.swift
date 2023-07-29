c74b4c0ddc93cdf729126adff8af468827bcdf0b
Tucker Dickson <nathan-dickson@uiowa.edu>
2023-07-29 10:43:37 -0500

    Days 16 and 17

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tucker Dickson on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
