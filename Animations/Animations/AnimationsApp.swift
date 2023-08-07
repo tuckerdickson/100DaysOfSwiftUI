07a7dac46dac745ea48303718b9e71a9dde1dfca
Tucker Dickson <nathan-dickson@uiowa.edu>
2023-08-07 18:50:03 -0400

    day 32

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Tucker Dickson on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
