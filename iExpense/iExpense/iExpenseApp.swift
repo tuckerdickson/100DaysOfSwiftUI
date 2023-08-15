2af5cf105c388d43bd8bf08bf1bfdb57bbef9956
Nathan Dickson <nathandickson@Nathans-MacBook-Air.local>
2023-08-15 08:14:19 -0400

    day 36

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Nathan Dickson on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
