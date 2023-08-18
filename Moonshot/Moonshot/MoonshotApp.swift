e4f29569f2fb78c71c4cf9560d602da6a9a2344f
Nathan Dickson <nathandickson@Nathans-MacBook-Air.local>
2023-08-18 07:19:43 -0400

    day 39

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Nathan Dickson on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
